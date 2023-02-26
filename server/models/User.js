1f8b7bf7107878cecdd34598bef08960bad34552
hoatnnguyen <selenatn.nguyen@gmail.com>
2023-02-26 00:05:53 -0800

    create User Model

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;

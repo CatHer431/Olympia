87d3a6a6c7bebf1688e3502435f739ef1506b16a
hoatnnguyen <selenatn.nguyen@gmail.com>
2023-02-26 15:28:53 -0800

    add validator library and add validator for email

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,12 +16,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please enter an email"],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, "Please enter a valid email"]
     },
     password: {
         type: String,
         required: [true, "Please enter a password"],
-        minlength: 6
+        minlength: [6, "Minimum password length is 6 characters"]
     }
 });
 
